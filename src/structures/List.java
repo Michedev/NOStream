7d1fd8d959aec80c35a9af99bb63b6ae0396888b
Michele De Vita <mdv1994@gmail.com>
2016-12-25 15:54:14 +0100

    Refactored tests and override return type in List

--- src/structures/List.java
+++ src/structures/List.java
@@ -3,89 +3,62 @@ package structures;
 import utils.Pair;
 
 import java.util.Comparator;
+import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.function.BiPredicate;
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+
+import functions.ConsumerIndexed;
 
 /**
  * Created by mikedev on 13/07/16.
  */
 public interface List<T> extends java.util.List<T>, Collection<T> {
-/*
 
-    @Override
-    default List<T> distinct() {
-        return ((List<T>) Collection.super.distinct());
-    }
+	@Override
+	List<T> distinct();
+	@Override
+	List<T> filter(Predicate<T> predicate);
+	@Override
+	 List<T> filterIndexed(BiPredicate<T, Integer> predicate) ;
+	@Override
+	 <E> Map<E, ArrayList<T>> groupBy(Function<T, E> thisFuct) ;
+	@Override
+	 List<T> intersection(java.util.Collection<T> List) ;
+	@Override
+	 <R> List<R> map(Function<T, R> mapper) ;
+	@Override
+	 <R> List<R> mapIndexed(BiFunction<T, Integer, R> mapper) ;
+	@Override
+	 List<T> orderBy(Comparator<T> comparator) ;
+	@Override
+	 List<T> orderDecreasingBy(Comparator<T> comparator) ;
 
-    @Override
-    default List<T> filter(Predicate<T> predicate) {
-        return ((List<T>) Collection.super.filter(predicate));
-    }
-    
-    @Override
-    default List<T> filterIndexed(BiPredicate<T, Integer> predicate) {
-        return  ((List<T>) Collection.super.filterIndexed(predicate));
-    }
+	@Override
+	 List<T> reverse() ;
 
-    @Override
-    default List<T> filterNotNull() {
-        return (List<T>) Collection.super.filterNotNull();
-    }
+	@Override
+	 List<T> take(int n) ;
 
-    @Override
-    default List<T> intersection(java.util.Collection<T> collection) {
-        return ((List<T>) Collection.super.intersection(collection));
-    }
+	@Override
+	 List<T> takeLast(int n) ;
 
-    @Override
-    default  <R> List<R> map(Function<T, R> mapper) {
-        return ((List<R>) Collection.super.map(mapper));
-    }
+	@Override
+	 List<T> union(java.util.Collection<T> List) ;
 
-    @Override
-    default <R>  List<R> mapIndexed(BiFunction<T, Integer, R> mapper) {
-        return ((List<R>) Collection.super.mapIndexed(mapper));
-    }
 
-    @Override
-    default List<T> orderBy(Comparator<T> comparator) {
-        return ((List<T>) Collection.super.orderBy(comparator));
-    }
+	@Override
+	 List<Pair<T, Integer>> zipIndexed() ;
 
-    @Override
-    default List<T> orderDecreasingBy(Comparator<T> comparator) {
-        return ((List<T>) Collection.super.orderDecreasingBy(comparator));
-    }
 
-    @Override
-    default List<T> reverse() {
-        return ((List<T>) Collection.super.reverse());
-    }
+	@Override
+	 <X> List<Pair<T, X>> zipWith(java.util.Collection<X> other) ;
 
-    @Override
-    default List<T> take(int n) {
-        return ((List<T>) Collection.super.take(n));
-    }
+	@Override
+	 List<T> filterNotNull() ;
 
-    @Override
-    default List<T> takeLast(int n) {
-        return ((List<T>) Collection.super.takeLast(n));
-    }
-
-    @Override
-    default List<T> union(java.util.Collection<T> collection) {
-        return ((List<T>) Collection.super.union(collection));
-    }
-
-    @Override
-    default List<Pair<T, Integer>> zipIndexed() {
-        return ((List<Pair<T, Integer>>) Collection.super.zipIndexed());
-    }
-
-    @Override
-    default <X> List<Pair<T, X>> zipWith(Collection<X> other) {
-        return ((List<Pair<T, X>>) Collection.super.zipWith(other));
-    }*/
+	
 }
